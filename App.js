28ef897cbc7843dc9bdd912731a692d8faa53d78
Crystal Mora <ChibiMora@users.noreply.github.com>
2018-06-16 00:11:23 -0400

    Iniital Commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2017-present, Viro, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default class AppRoot extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+    }
+  }
+
+  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
+  // if you are building a specific type of experience.
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    )
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import AppRoot from './App.js';
+
+AppRegistry.registerComponent('PuppySender', () => AppRoot);
+
+// The below line is necessary for use with the TestBed App
+//AppRegistry.registerComponent('ViroSample', () => App);
