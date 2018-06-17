079a6cd95a90c88c02fb62838992cdf826f4d874
Crystal Mora <ChibiMora@users.noreply.github.com>
2018-06-17 15:45:55 -0400

    added all the puppy images

--- js/components/DetailsScreen.js
+++ js/components/DetailsScreen.js
@@ -12,15 +12,21 @@ export default class DetailsScreen extends React.Component {
 
     this.state = {
       dogs: [
-        {id: 1, name: 'Beagle', image: require('../images/beagle.png'), arFile: 'sample'},
-        {name: 'Corgi', image: require('../images/corgi.png'), arFile: 'fdls'}
+        {id: 1, name: 'Beagle', image: require('../images/beagle.png'), arFile: require('../res/beagle/Mesh_Beagle.obj')},
+        {id: 2, name: 'Corgi', image: require('../images/corgi.png'), arFile: require('../res/corgi/model.obj')},
+        {id: 3, name: 'Pupper with collar', image: require('../images/dogwithcollar.png'), arFile: require('../res/dogwithcollar/dogwithcollar.obj')},
+        {id: 4, name: 'Flying pup', image: require('../images/flyinpup.png'), arFile: require('../res/flyingPup/model.obj')},
+        {id: 5, name: 'German Shepard', image: require('../images/germanshepard.png'), arFile: require('../res/germanshepard/model.obj')},
+        {id: 6, name: 'Pupper', image: require('../images/normpup.png'), arFile: require('../res/norm dog/Booboo.obj')},
+        {id: 7, name: 'Water Dog', image: require('../images/otter.png'), arFile: require('../res/RiverOtter/RiverOtter.obj')},
+        {id: 8, name: 'Poodle', image: require('../images/poodle.png'), arFile: require('../res/Poodle/Poodle_01.obj')},
+        {id: 9, name: 'Pug', image: require('../images/pug.png'), arFile: require('../res/pug/model.obj')},
+        {id: 10, name: 'Pylon Pug', image: require('../images/pugwithcone.png'), arFile: require('../res/pylonPug/model.obj')},
+        {id: 11, name: 'Doggie', image: require('../images/regdog.png'), arFile: require('../res/regpup/dog.obj')}
       ]
     };
   }
 
-  // _selectedPuppy = (event) => {
-  //   this.setState({selectedPuppy: event.nativeEvent.})
-  // }
 
   render() {
     const { navigation } = this.props;
--- js/components/PreviewScreen.js
+++ js/components/PreviewScreen.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {ViroARScene, ViroText, ViroConstants, ViroBox, ViroMaterials, ViroMaterials, ViroAmbientLight, ViroSpotLigt} from 'react-viro';
 
 export default class PreviewScreen extends React.Component {
-    
+    constructor (){
+        super();
+        this.state = {
+            text: 'Initializing AR....'
+        }
+        this._onInitialized = this._onInitialized.bind(this);
+    }
+    _onInitialized(state) {
+        if(state == ViroConstants.TRACKING_NORMAL){
+            this.setState({
+                text: 
+            })
+        }
+    }
+    render(){
+        const {navigation} = this.props;
+        const name = navigation.getParam('name');
+        return (
+            <ViroARScene onTrackingUpdated={this._onInitialized} >
+                <ViroText text={}
+            </ViroARScene>
+        )
+    }
 }
